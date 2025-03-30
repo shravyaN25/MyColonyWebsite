0bc60102b5d9aa81dea5cd037c0335a90763e5b4
Shravya N <109436935+shravyaN25@users.noreply.github.com>
2025-03-30 08:45:22 +0530

    Update page.jsx
    update

--- app/page.jsx
+++ app/page.jsx
@@ -389,9 +389,6 @@ export default function HomePage() {
                 ))}
               </ul>
             </div>
-
-            
-
           <div className="border-t border-gray-800 mt-12 pt-8 text-center text-gray-400">
             <p>&copy; {new Date().getFullYear()} Made with ❤️ by Shravya N</p>
           </div>
